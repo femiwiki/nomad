06836ad5b6fc59575a3afc393e032428c4b6c5b2
Lens0021 / Leslie <lorentz0021@gmail.com>
2024-10-06 18:06:33 +0900

    Use EBS for Caddy on the green cluster (#298)

--- terraform/volumes.tf
+++ terraform/volumes.tf
@@ -14,7 +14,31 @@ resource "nomad_job" "plugin-ebs-nodes" {
   }
 }
 
+resource "nomad_job" "plugin_ebs_controller_green" {
+  provider = nomad.test
+  jobspec  = file("../jobs/plugin-ebs-controller.nomad")
+
+  hcl2 {
+    allow_fs = true
+  }
+}
+
+resource "nomad_job" "plugin_ebs_nodes_green" {
+  provider = nomad.test
+  jobspec  = file("../jobs/plugin-ebs-nodes.nomad")
+
+  hcl2 {
+    allow_fs = true
+  }
+}
+
 data "nomad_plugin" "ebs" {
+  plugin_id        = "aws-ebs0"
+  wait_for_healthy = true
+}
+
+data "nomad_plugin" "ebs_green" {
+  provider         = nomad.test
   plugin_id        = "aws-ebs0"
   wait_for_healthy = true
 }
@@ -46,6 +70,20 @@ resource "nomad_csi_volume_registration" "caddycerts" {
   volume_id   = "caddycerts"
   name        = "caddycerts"
   external_id = data.terraform_remote_state.aws.outputs.ebs_caddycerts_id
+
+  capability {
+    access_mode     = "single-node-writer"
+    attachment_mode = "file-system"
+  }
+}
+
+resource "nomad_csi_volume_registration" "caddycerts_green" {
+  provider    = nomad.test
+  depends_on  = [data.nomad_plugin.ebs_green]
+  plugin_id   = "aws-ebs0"
+  volume_id   = "caddycerts_green"
+  name        = "caddycerts_green"
+  external_id = data.terraform_remote_state.aws.outputs.ebs_caddycerts_green_id
 
   capability {
     access_mode     = "single-node-writer"
